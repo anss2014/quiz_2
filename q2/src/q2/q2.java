e54af7861ced7b58e3295cd66a7b028c1ea8e15c
anss <anss@DESKTOP-0U7B6MM>
2017-02-26 17:11:21 -0500

    123

---
+++ q2/src/q2/q2.java
@@ -0,0 +1,35 @@
+package q2;
+
+import java.util.Scanner;
+
+public class q2 {
+
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		double initialCost = 0;
+		double percentInc = 0;
+		double totalCost = 0;
+		
+		System.out.println("Initial tuition cost?");
+		initialCost = scan.nextDouble();
+
+		System.out.println("Percentage increase in tuition?");
+		percentInc = scan.nextDouble()/100; 
+		
+		totalCost = initialCost; 
+
+		double cost = initialCost; 
+		
+		for (int i = 0; i < 3; i++) {
+			cost = (cost + percentInc*cost);
+			totalCost = (cost + totalCost);
+		}
+		
+		System.out.println("Cost of tuition:");		
+		System.out.println(totalCost);
+		
+	}
+
+}
